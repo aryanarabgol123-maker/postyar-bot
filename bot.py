d7c754069ebb09a3642fad9a7ada724e5b094fb0
aryanarabgol123-maker <aryanarabgol123@gmail.com>
2026-02-10 07:07:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام آرین 👋\nربات پایتونی با موفقیت اجرا شد ✅")
+    await update.message.reply_text("سلام آرین 👋 بات با موفقیت اجرا شد ✅")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
-    token = os.getenv("BOT_TOKEN")
-
-    app = ApplicationBuilder().token(token).build()
-    app.add_handler(CommandHandler("start", start))
-
-    print("Bot is running...")
-    app.run_polling()
+    main()
